549f5c66a942b002cc524d8a1330fbc2e0873ba2
Scooter)Ok <scooterok@gmail.com>
2018-05-01 13:20:41 +0300

    up

---
+++ assets/custom-scripts.js.liquid
@@ -0,0 +1,11 @@
+(function($){
+//readmore
+$('.fulltext').each(function(){
+$('.fulltext').hide();
+$('.shown .readmore').click(function (event) {
+event.preventDefault();
+$(this).parent().find('.fulltext').slideToggle('slow');
+$(this).text($(this).text() == 'Read less' ? 'Read more' : 'Read less');
+});
+});
+})(jQuery);
--- assets/main.js
+++ assets/main.js
@@ -1,20 +1,89 @@
+var widgetLink = {};
+
+/* Cookies API made in Burdosik */
+
+function GetCookie (name){
+    var arg = name + "=";
+    var alen = arg.length;
+    var clen = document.cookie.length;
+    var i = 0;
+    while (i < clen)
+    {
+        var j = i + alen;
+        if (document.cookie.substring(i, j) === arg)
+            return getCookieVal (j);
+        i = document.cookie.indexOf(" ", i) + 1;
+        if (i === 0) break;
+    }
+    return null;
+}
+function SetCookie(name, value) {
+    var argv = SetCookie.arguments;
+    var argc = SetCookie.arguments.length;
+    var expires = (argc > 2) ? argv[2] : null;
+    var path = '/';//(argc > 3) ? argv[3] : null;
+    var domain = (argc > 4) ? argv[4] : null;
+    var secure = (argc > 5) ? argv[5] : false;
+    var ExpireDate;
+    if ((expires + '').length < 6) {   //this arg is countDays
+        ExpireDate = new Date();
+        ExpireDate.setTime(ExpireDate.getTime() + (expires * 24 * 3600 * 1000));
+    } else { // this arg is expire Timestamp
+        ExpireDate = new Date(expires);
+    }
+    document.cookie = name + "=" + escape(value) +
+        ((expires === null) ? "" : ("; expires=" + ExpireDate.toGMTString())) +
+        ((path === null) ? "" : ("; path=" + path)) +
+        ((domain === null) ? "" : ("; domain=" + domain)) +
+        ((secure) ? "; secure" : "");
+}
+function DeleteCookie(name) {
+    var expt = new Date();
+    expt.setTime(expt.getTime() - 1000000000);  // This cookie is history (changed -1 to make it previous time)
+    var cval = GetCookie(name);
+    document.cookie = name + "=" + cval + "; expires=" + expt.toGMTString() + "; path=/;";
+}
+function getCookieVal(offset) {
+    var endstr = document.cookie.indexOf(";", offset);
+    if (endstr == -1) {
+        endstr = document.cookie.length;
+    }
+    return unescape(document.cookie.substring(offset, endstr));
+}
+
+
+
+
 $(document).ready(function() {
-	$('.product__descriptions_speciﬁcations-tabs li').click(function(e) {
+    $('header .header__mobile_menu-but').click(function(e){
+		$('body').addClass('show-menu');
+    });
+  	$('header .header__mobile_menu .close').click(function(e){
+		$('body').removeClass('show-menu');
+    });
+  	
+	$('.product').on('click', '.product__descriptions_speciﬁcations-tabs li', function(e){
 		var target = $(this).attr('data-link');
 		$('.product__descriptions_speciﬁcations-tabs li').removeClass('current');
 		$(this).addClass('current');
 		$('.product__descriptions_speciﬁcations-list > li').removeClass('current');
 		$('.product__descriptions_speciﬁcations-list #'+target).addClass('current');
+	});	
+
+	$('#get-repeater-but').click(function(event) {
+		var body = $("html, body");
+		body.stop().animate({scrollTop:0}, 500, 'swing');
 	});
 
 	$('body').on('click', '.modal', function(e){
 		if($(e.target).hasClass('close')){
-			$('.modal').hide();	
-		}else if($(e.target).closest('.modal__body').length){			
-			e.preventDefault();
-			return false;						
+			$('.modal').fadeOut();
+			$('body').removeAttr('style');
 		}
-		$('.modal').hide();
+		if(!$(e.target).closest('.modal__body').length){
+			$('.modal').fadeOut();
+			$('body').removeAttr('style');
+		}		
 	});
 	$('.body').on('click', '.modal i.close', function(e){
 		$('.modal').hide();
@@ -34,8 +103,8 @@ $(document).ready(function() {
 			val--;
 			$(this).closest('.card__list_item-quantity').find('input').val(val);
 			if(val == 1)$(this).addClass('disabled');
-
-		}		
+		}
+		changeItemCard($(this));
 	});
 
 	$('#card').on('click', '.card__footer_delivery-switcher', function(e){
@@ -52,13 +121,31 @@ $(document).ready(function() {
 
 	$('.accessories').on('click', '.accessories__filter-switcher div', function(e){
 		$(this).toggleClass('list cards');
-		$('.accessories__collection').toggleClass('cadrs list');
+		$('.accessories__collection').toggleClass('list cards');
 	});
 
-	$('main.technical-support').on('click', '.technical-support__list h3', function(e){
-		$('.technical-support__list .item__content').slideUp(150);
-		$('.technical-support__list h3').removeClass('show');
-		$(this).closest('.technical-support__list_item').addClass('show');
-		$(this).closest('.technical-support__list_item').find('.item__content').slideDown(150);
+	$('body').on('mouseenter', '.widget-tooltip', function(e){
+		var l = $(window).width() - $(this).offset().left - 65;
+		$(this).find('.widget-tooltip__text').css('maxWidth', l );
+		console.log(l);
+	});
+
+	$('body').on('click', '.header__nav_params-card', function(e){
+		var count = $(this).attr('data-count');
+		if(count > 0){
+          if($(window).width() > 1000){
+          	$('#card').fadeIn(150);
+			$('body').css('overflow', 'hidden');	
+          }else{
+          	window.location.href = '/cart';
+          }
+		}		
+	});
+	$('body').on('click', '#card .card__list_item-remove i', function(e){
+		var id = $(this).closest('.card__list_item').attr('data-id');
+		var item = {};
+		item[id] = 0;
+		removeItemCard(item);
 	});
 });
+
